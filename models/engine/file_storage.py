8949582cb614487d3afe8b4417ea248a250e2298
Naoufal hdr <naoufalhdr@gmail.com>
2024-04-30 14:59:31 +0100

    Task 5: update file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,7 +9,7 @@ class FileStorage:
     __objects = {}
 
     def all(self, cls=None):
-        """Returns a dictionary of models currently in storage"""
+        """Returns the list of objects of one type of class"""
         storage = FileStorage.__objects
         if cls:
             filter_objects = {
