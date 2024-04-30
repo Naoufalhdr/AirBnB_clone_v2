1cf8f07c5f38584d32313e7603e2c44993ad2921
Naoufal hdr <naoufalhdr@gmail.com>
2024-04-30 11:35:26 +0100

    Task 4: fix a typo error

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -8,5 +8,5 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
 -- Grant SELECT privilege on performance_scheme databse
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 
