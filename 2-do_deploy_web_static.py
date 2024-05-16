aeac806d91b0a3b11ccb8de7f32f5f8c07bbc6a4
Naoufal hdr <naoufalhdr@gmail.com>
2024-05-16 20:19:33 +0100

    Task 2 & Task 3: fix pycodestyle

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
-# Fabfile to distribute an archive to a web server.
+"""
+Fabric script to distribute an archive to web servers using the function
+do_deploy.
+"""
 import os.path
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
+from fabric.api import env, put, run
 
 env.hosts = ["54.157.186.100", "52.86.133.13"]
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
     """
+    Distributes an archive to web servers.
+    Returns True if all operations have been done correctly, otherwise False.
+    """
+
     if os.path.isfile(archive_path) is False:
         return False
     file = archive_path.split("/")[-1]
     name = file.split(".")[0]
 
+    # Upload the archive to the web server
     if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
+
+    # Uncompress the archive on the web server
     if run("rm -rf /data/web_static/releases/{}/".
            format(name)).failed is True:
         return False
@@ -33,63 +34,27 @@ def do_deploy(archive_path):
     if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
            format(file, name)).failed is True:
         return False
+
+    # Delete the archive from the web server
     if run("rm /tmp/{}".format(file)).failed is True:
         return False
+
+    # Move the contents of the uncompressed folder to its parent dir
     if run("mv /data/web_static/releases/{}/web_static/* "
            "/data/web_static/releases/{}/".format(name, name)).failed is True:
         return False
+
+    # Remove the now empty web_static dir
     if run("rm -rf /data/web_static/releases/{}/web_static".
            format(name)).failed is True:
         return False
+
+    # Delete the symbolic link /data/web_static/current
     if run("rm -rf /data/web_static/current").failed is True:
         return False
+
+    # Create a new symbolic link /data/web_static/current
     if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
            format(name)).failed is True:
         return False
     return True
-"""
-#!/usr/bin/python3
-Fabric script to distribute an archive to web servers using the function
-do_deploy.
-from fabric.api import env, put, run
-import os.path
-
-env.hosts = ['54.157.186.100', '52.86.133.13']
-
-
-def do_deploy(archive_path):
-    Distributes an archive to web servers.
-    Returns True if all operations have been done correctly, otherwise False.
-    if not os.path.exists(archive_path):
-        return False
-
-    try:
-        # Upload the archive to the web server
-        put(archive_path, '/tmp/')
-
-        # Uncompress the archive on the web server
-        archive_filename = os.path.basename(archive_path)
-        folder_name = f"/data/web_static/releases/{archive_filename[:-4]}"
-        run(f"mkdir -p {folder_name}")
-        run(f"tar -xzf /tmp/{archive_filename} -C {folder_name}/")
-
-        # Delete the archive from the web server
-        run(f"rm /tmp/{archive_filename}")
-
-        # Move the contents of the uncompressed folder to its parent dir
-        run(f"mv {folder_name}/web_static/* {folder_name}/")
-
-        # Remove the now empty web_static dir
-        run(f"rm -rf {folder_name}/web_static")
-
-        # Delete the symbolic link /data/web_static/current
-        run("rm -rf /data/web_static/current")
-
-        # Create a new symbolic link /data/web_static/current
-        run(f"ln -s {folder_name}/ /data/web_static/current")
-
-        return True
-
-    except Exception as e:
-        return False
-"""
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -39,39 +39,50 @@ def do_deploy(archive_path):
     Distributes an archive to web servers.
     Returns True if all operations have been done correctly, otherwise False.
     """
-    if not os.path.exists(archive_path):
+
+    if os.path.isfile(archive_path) is False:
+        return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
+
+    # Upload the archive to the web server
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
 
-    try:
-        # Upload the archive to the web server
-        put(archive_path, '/tmp/')
-
-        # Uncompress the archive on the web server
-        archive_filename = os.path.basename(archive_path)
-        folder_name = f"/data/web_static/releases/{archive_filename[:-4]}"
-        run("mkdir -p {}".format(folder_name))
-        run("tar -xzf /tmp/{} -C {}".format(archive_filename, folder_name))
-
-        # Delete the archive from the web server
-        run("rm /tmp/{}".format(archive_filename))
-
-        # Move the contents of the uncompressed folder to its parent dir
-        run("mv {}/web_static/* {}".format(folder_name, folder_name))
-
-        # Remove the now empty web_static dir
-        run("rm -rf {}/web_static".format(folder_name))
-
-        # Delete the symbolic link /data/web_static/current
-        run("rm -rf /data/web_static/current")
-
-        # Create a new symbolic link /data/web_static/current
-        run("ln -s {} /data/web_static/current".format(folder_name))
-
-        print("New version deployed!")
-        return True
-
-    except Exception as e:
+    # Uncompress the archive on the web server
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
         return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+
+    # Delete the archive from the web server
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+
+    # Move the contents of the uncompressed folder to its parent dir
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+
+    # Remove the now empty web_static dir
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+
+    # Delete the symbolic link /data/web_static/current
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+
+    # Create a new symbolic link /data/web_static/current
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
+    return True
 
 
 def deploy():
