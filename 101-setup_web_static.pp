9a140e28ced2263a4c47a98ce247c8a70ef3c2ed
Naoufal hdr <naoufalhdr@gmail.com>
2024-05-16 22:16:52 +0100

    Task 5: update file

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,62 +1,91 @@
-# Puppet Manifest to set up web servers for the deployment of web_static.
+# Configures a web server for deployment of web_static.
 
-# Install Nginx if not already installed
+# Nginx configuration file
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
+
 package { 'nginx':
-  ensure => installed,
-}
+  ensure   => 'present',
+  provider => 'apt'
+} ->
 
-# Create necessary directories if they don't exist
-file { ['/data/web_static/releases/test/', '/data/web_static/shared/']:
-  ensure => directory,
-}
+file { '/data':
+  ensure  => 'directory'
+} ->
 
-# Create index.html file
+file { '/data/web_static':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases/test':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/shared':
+  ensure => 'directory'
+} ->
+
 file { '/data/web_static/releases/test/index.html':
-  ensure  => present,
-  content => '<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>',
-}
+  ensure  => 'present',
+  content => "Holberton School Puppet\n"
+} ->
 
-# Create or recreate the symbolic link
 file { '/data/web_static/current':
-  ensure => link,
-  target => '/data/web_static/releases/test',
-  force  => true,
+  ensure => 'link',
+  target => '/data/web_static/releases/test'
+} ->
+
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
-# Set ownership recursively to the ubuntu user and group
-file { '/data/':
-  ensure  => directory,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  recurse => true,
-}
+file { '/var/www':
+  ensure => 'directory'
+} ->
 
-# Update Nginx configuration
+file { '/var/www/html':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "Holberton School Nginx\n"
+} ->
+
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n"
+} ->
+
 file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => "server {
-	listen 80;
-	listen [::]:80;
+  ensure  => 'present',
+  content => $nginx_conf
+} ->
 
-	server_name _;
-
-	location /hbnb_static/ {
-		alias /data/web_static/current/;
-		index index.html;
-	}
-}
-",
-  notify  => Service['nginx'],
-}
-
-# Restart nginx
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
+exec { 'nginx restart':
+  path => '/etc/init.d/'
 }
