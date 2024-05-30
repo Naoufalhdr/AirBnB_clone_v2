cbc2b37c1a3f3708510902ed00b27ee5be5002a8
Naoufalhdr <naoufalhdr96@gmail.com>
2024-05-30 21:34:57 +0100

    Task 1: HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-""" Start a simple Flask web application """
+"""
+Start a simple Flask web application with two routes:
+    - /: displays "Hello HBNB!"
+    - /hbnb : displays "HBNB"
+"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,6 +12,11 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
 
 
 if __name__ == "__main__":
